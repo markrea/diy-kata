df5c5ef6795be03e8391c3d62126008f5dcdbb81
Mark Rea <markrea366@gmail.com>
2020-03-10 11:16:06 +0000

    finished reachDestination#

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,8 @@
-const reachDestination = (distance, speed) => {};
+const reachDestination = (distance, speed) => {
+    journeyTime = speed / distance;
+// double journeyTime, round to next whole number then halve 
+    const rounded = Math.ceil(journeyTime * 2) / 2; 
+    return ("I should be there in " + rounded + " hours.")
+};
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,7 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(10, 44)).toBe("I should be there in 4.5 hours.")
+  });
 });
