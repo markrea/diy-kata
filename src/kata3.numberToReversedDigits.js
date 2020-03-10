272245aeeec69090cb2f60a1f9bc239a3c380cdf
Mark Rea <markrea366@gmail.com>
2020-03-10 09:13:53 +0000

    finished numberToReversedString

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,7 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+    const numbersString = number.toString();
+    const numbersArray = numbersString.split("").reverse();
+    return numbersArray.map(Number);
+};
 
 module.exports = numberToReversedDigits;
