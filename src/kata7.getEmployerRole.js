7636aab86d82949814131304ab8e862a7d8939e9
Mark Rea <markrea366@gmail.com>
2020-03-14 17:15:27 +0000

    finished getEmployeeRole

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,11 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+  return employees.find(employee => {
+    return employee.name === employeeName;
+  }).role;
+};
 
 module.exports = getEmployerRole;
+
+/* used .find() to pull the value of name in the employees
+array and test if it is equal to employeeName, then 
+returns the role*/
--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,30 @@
 const { getEmployerRole } = require("../src");
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  const employees = [
+    {
+      name: "Satti",
+
+      role: "Developer"
+    },
+    {
+      name: "Jenny",
+
+      role: "Sales Associate"
+    },
+    {
+      name: "Javid",
+
+      role: "Human Recommended Reading Assistant"
+    }
+  ];
+  test("returns the employee's role in the company", () => {
+    expect(getEmployerRole("Satti", employees)).toEqual("Developer");
+  });
+  test("returns the employee's role in the company", () => {
+    expect(getEmployerRole("Jenny", employees)).toEqual("Sales Associate");
+  });
+  test("returns the employee's role in the company", () => {
+    expect(getEmployerRole("Javid", employees)).toEqual("Human Recommended Reading Assistant");
+  });
 });
