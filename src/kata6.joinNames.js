2509b33d08ad732da878608d693265a37687920c
Mark Rea <markrea366@gmail.com>
2020-03-14 16:09:29 +0000

    finished join names

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,12 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+  // put object name values in array
+  const nameArray = namesObj.map(x => x.name);
+  // remove last item from array
+  const lastItem = nameArray.pop();
+  // create string off remaining names separated by comma and space
+  const nameString = nameArray.join(", ");
+
+  return `${nameString} & ${lastItem}`;
+};
 
 module.exports = joinNames;
